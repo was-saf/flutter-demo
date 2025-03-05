02729c27779edae352ca64a076e694b943c597ff
Wassaf <wassafahmed123@gmail.com>
2025-03-06 03:53:31 +0500

    DOONE NO

--- lib/views/pages/welcome_page.dart
+++ lib/views/pages/welcome_page.dart
@@ -8,57 +8,59 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: EdgeInsets.only(top: 40.0),
-        child: Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              Image.asset('assets/images/logo.png'),
-              FittedBox(
-                child: Text(
-                  'Student Handbook',
-                  style: TextStyle(
-                    fontSize: 500,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.teal,
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.only(top: 40.0),
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Image.asset('assets/images/logo.png'),
+                FittedBox(
+                  child: Text(
+                    'Student Handbook',
+                    style: TextStyle(
+                      fontSize: 500,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.teal,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 20),
-              FilledButton(
-                style: FilledButton.styleFrom(
-                  minimumSize: Size(double.infinity, 40.0),
+                SizedBox(height: 20),
+                FilledButton(
+                  style: FilledButton.styleFrom(
+                    minimumSize: Size(double.infinity, 40.0),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return SignupPage();
+                        },
+                      ),
+                    );
+                  },
+                  child: Text('Get Started'),
                 ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return SignupPage();
-                      },
-                    ),
-                  );
-                },
-                child: Text('Get Started'),
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  minimumSize: Size(double.infinity, 40.0),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    minimumSize: Size(double.infinity, 40.0),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return LoginPage();
+                        },
+                      ),
+                    );
+                  },
+                  child: Text('Login'),
                 ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return LoginPage();
-                      },
-                    ),
-                  );
-                },
-                child: Text('Login'),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
